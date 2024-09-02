00173b57596f53f17a44a3784662ff598dee916d
Tagert <mini.andrius@gmail.com>
2024-09-02 21:09:25 +0300

    :sparkles: feat(SideNavbar): Experimenting with MUI lib.

--- client/README.md
+++ client/README.md
@@ -1,3 +1,5 @@
-# React + TypeScript + Vite
+# React + TypeScript + Vite + MUI.
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+I am trying to learn how to use the MUI lib.
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,47 @@
+import { Box, Grid2, Paper, Typography } from "@mui/material";
+import Divider from "@mui/material/Divider";
+
 const App = () => {
-  return <main className="test"></main>;
+  return (
+    <Box component="section" display="flex" height="100vh">
+      <Paper
+        sx={{
+          position: "fixed",
+          display: "flex",
+          justifyContent: "center",
+          width: 250,
+          height: "calc(100% - 2rem)",
+          margin: 2,
+          padding: 2,
+          borderRadius: 3,
+          bgcolor: "#3d3f45",
+        }}
+      >
+        <Typography color="white" variant="h6" noWrap component="div">
+          Company name
+        </Typography>
+
+        <Divider />
+      </Paper>
+
+      <Grid2 container spacing={2} display="flex" flex={1} flexDirection="column" ml={35} p={2}>
+        <Grid2
+          size={12}
+          display="flex"
+          alignItems="center"
+          height={65}
+          px={2}
+          py={1}
+          borderRadius={3}
+          bgcolor="#f0f2f5"
+        >
+          Testing
+        </Grid2>
+
+        <Grid2 size={12}>Testing</Grid2>
+      </Grid2>
+    </Box>
+  );
 };
 
 export default App;
